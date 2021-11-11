1f1317eae8433a2afde4ef81345a83fc37aa8fc1
enoxy <enoxy@DESKTOP-8BHQKRR>
2021-11-11 13:40:03 +0200

    Добавьте файлы проекта.

---
+++ дз1/Program.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+
+namespace дз1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double x, y, z;
+            Console.WriteLine("Введите число x");
+            x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите чиcло y");
+            y = Double.Parse(Console.ReadLine());
+            z = Math.Sqrt(Math.Pow((2 + y), 2) + Math.Pow(Math.Sin(y + 5), 1 / 7)) / Math.Log(x + 1) - Math.Pow(y, 3);
+            Console.WriteLine("z={0:F2}", z);
+        }
+`    }
+}
